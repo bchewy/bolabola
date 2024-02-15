512f8276422c230ad89532f991450a93d65c6f23
tanhaoen <nathanieltan1701@gmail.com>
2024-02-16 01:38:50 +0800

    Initialize SQS consumer

---
+++ queue/sqs_consume/sqs_consume.go
@@ -0,0 +1,49 @@
+package sqs_consume
+
+import (
+	"github.com/aws/aws-sdk-go/aws"
+	"github.com/aws/aws-sdk-go/aws/session"
+	"github.com/aws/aws-sdk-go/service/sqs"
+	"github.com/gorilla/websocket"
+	"log"
+	"queue/connection_manager"
+)
+
+func consumeMessages(sess *session.Session, queueUrl string, manager *connection_manager.ConnectionManager) {
+    svc := sqs.New(sess)
+
+    for {
+        result, err := svc.ReceiveMessage(&sqs.ReceiveMessageInput{
+            QueueUrl:            aws.String(queueUrl),
+            MaxNumberOfMessages: aws.Int64(1),
+            VisibilityTimeout:   aws.Int64(30),  // 30 seconds
+            WaitTimeSeconds:     aws.Int64(0),
+        })
+
+        if err != nil {
+            log.Println("Error", err)
+            return
+        }
+
+        if len(result.Messages) == 0 {
+            log.Println("Received no messages")
+            continue
+        }
+
+        // Assume the message body is the user ID
+        user_id := *result.Messages[0].Body
+
+        // Retrieve the connection
+        conn, ok := manager.GetConnection(user_id)
+        if !ok {
+            log.Printf("No connection found for user ID %s", user_id)
+            continue
+        }
+
+        // Send a message back to the client
+        if err := conn.WriteMessage(websocket.TextMessage, []byte("Your message has been dequeued")); err != nil {
+            log.Println(err)
+            return
+        }
+    }
+}
