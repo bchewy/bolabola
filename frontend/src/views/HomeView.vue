28c2d119d08e50f0b96a96e1f0ff5fdb6468094e
Brian Chew <brian@bchewy.com>
2024-02-26 01:57:39 +0800

    Vue starter

---
+++ frontend/src/views/HomeView.vue
@@ -0,0 +1,20 @@
+<template>
+  <div class="home-view">
+    <h1>Welcome to TicketBoost</h1>
+    <p>Start exploring and finding the best tickets for your favorite events!</p>
+  </div>
+</template>
+
+<script>
+export default {
+  name: "HomeView",
+  // Add your component's logic here
+}
+</script>
+
+<style scoped>
+.home-view {
+  text-align: center;
+  margin-top: 50px;
+}
+</style>
