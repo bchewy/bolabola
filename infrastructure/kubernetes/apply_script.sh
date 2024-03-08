f5d18206bf3de95ec1e6b21f364bfab11e251b58
Brian Chew <brian@bchewy.com>
2024-03-08 19:20:21 +0800

    apply

---
+++ infrastructure/kubernetes/apply_script.sh
@@ -0,0 +1,3 @@
+kubectl apply -f ./infrastructure/kubernetes/manifest/deployment/
+kubectl apply -f ./infrastructure/kubernetes/manifest/services/
+kubectl apply -f ./infrastructure/kubernetes/manifest/pvc/
