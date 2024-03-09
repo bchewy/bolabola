14dea6d1a631038605e100b6d431e6e13c4bcec3
Brian Chew <brian@bchewy.com>
2024-03-09 22:50:12 +0800

    Fixed an issue with mongoseed

---
+++ infrastructure/mongo/seed/entrypoint.sh
@@ -0,0 +1,12 @@
+#!/bin/bash
+# Wait for MongoDB to be ready
+# until mongo --host mongodb-service-name --eval "print(\"waited for connection\")"; do
+#   >&2 echo "MongoDB is unavailable - sleeping"
+#   sleep 1
+# done
+
+# Run mongoimport
+mongoimport --host mongodb --db matches --collection matches --type json --file /app/init.json --jsonArray
+
+# Keep container running if needed
+tail -f /dev/null
