4703e7d0b5a47e17de182469575295062455868e
Brian Chew <brian@bchewy.com>
2024-03-13 10:27:46 +0800

    Update entrypoint.sh script to handle empty database seeding

--- infrastructure/mongo/seed/entrypoint.sh
+++ infrastructure/mongo/seed/entrypoint.sh
@@ -5,8 +5,16 @@
 #   sleep 1
 # done
 
-# Run mongoimport
+# Run mongoimport, if matches db is empty; 
+# Currently the code here does not work due to the fact that.... mongo command is not found in the container!! - so dumb.. it's a mongo image..
+# mongo matches --eval "db.matches.count()" --quiet | grep 0
+# if [ $? -eq 0 ]; then
+#   echo "Database is empty. Seeding data..."
+#   mongoimport --host mongodb --db matches --collection matches --type json --file /app/init.json --jsonArray
+# else
+#   echo "Database is not empty. Skipping seeding..."
+# fi
 mongoimport --host mongodb --db matches --collection matches --type json --file /app/init.json --jsonArray
 
 # Keep container running if needed
-# tail -f /dev/null
+tail -f /dev/null
