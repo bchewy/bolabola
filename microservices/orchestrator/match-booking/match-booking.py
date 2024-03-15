c7028ea8f6668bff3242f22bf1a8b79a4b0d0666
Brian Chew <brian@bchewy.com>
2024-03-15 14:11:45 +0800

    multi tread

--- microservices/orchestrator/match-booking/match-booking.py
+++ microservices/orchestrator/match-booking/match-booking.py
@@ -1,5 +1,25 @@
 from flask import Flask
 import pika
+from threading import Thread
+
+####### RabbitMQ things #######
+def start_rabbitmq_consumer():
+    def callback(ch, method, properties, body):
+        pass
+
+    credentials = pika.PlainCredentials('ticketboost', 'veryS3ecureP@ssword')
+    parameters = pika.ConnectionParameters('rabbitmq', 5672, '/', credentials)
+    connection = pika.BlockingConnection(parameters)
+    channel = connection.channel()
+    
+    channel.queue_declare(queue='match', durable=True)
+    channel.basic_consume(queue='match', on_message_callback=callback, auto_ack=True)
+    channel.start_consuming()
+
+def run_consumer_thread():
+    consumer_thread = Thread(target=start_rabbitmq_consumer)
+    consumer_thread.daemon = True 
+    consumer_thread.start()
 
 app = Flask(__name__)
 
