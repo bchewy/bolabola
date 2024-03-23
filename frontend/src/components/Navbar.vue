51723672f72cf19dfc61506f4b72da65df9b3419
CarissaChia <133015320+CarissaChia@users.noreply.github.com>
2024-03-23 15:58:29 +0800

    navbar update

--- frontend/src/components/Navbar.vue
+++ frontend/src/components/Navbar.vue
@@ -120,7 +120,7 @@
                             <span class="text-evenlighter">Welcome, {{ user.name }}!</span>
                         </a>
                         <ul class="dropdown-menu bg-lightblue2 text-evenlighter" aria-labelledby="navbarDropdown">
-                            <!-- <li>
+                            <li>
                                 <router-link class="dropdown-item text-light" to="/profile">
                                     <svg xmlns="http://www.w3.org/2000/svg" width="24" height="24" viewBox="0 0 24 24"
                                         style="fill: rgba(255, 255, 255, 1);transform: ;msFilter:;">
@@ -128,7 +128,7 @@
                                             d="M12 2C6.579 2 2 6.579 2 12s4.579 10 10 10 10-4.579 10-10S17.421 2 12 2zm0 5c1.727 0 3 1.272 3 3s-1.273 3-3 3c-1.726 0-3-1.272-3-3s1.274-3 3-3zm-5.106 9.772c.897-1.32 2.393-2.2 4.106-2.2h2c1.714 0 3.209.88 4.106 2.2C15.828 18.14 14.015 19 12 19s-3.828-.86-5.106-2.228z">
                                         </path>
                                     </svg> Profile</router-link>
-                            </li> -->
+                            </li>
                             <!-- <li>
                                 <router-link class="dropdown-item text-light" to="/verify">
                                     <svg xmlns="http://www.w3.org/2000/svg" width="24" height="24" viewBox="0 0 24 24"
