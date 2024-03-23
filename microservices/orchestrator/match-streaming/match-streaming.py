240fdbc71f9f2106e6a6a84a9df21c9a43c13ead
Brian Chew <brian@bchewy.com>
2024-03-23 10:57:02 +0800

    Feat: Match-streaming retrieve match works!

--- microservices/orchestrator/match-streaming/match-streaming.py
+++ microservices/orchestrator/match-streaming/match-streaming.py
@@ -14,8 +14,8 @@ def index():
     return "Match Streaming Service.. alive"
 
 
-videoasset_url = "http://kong:8000/api/v1/videoasset/"
-match_url = "http://kong:8000/api/v1/match/"
+VIDEOASS_URL = "http://kong:8000/api/v1/videoasset/"
+MATCH_URL = "http://kong:8000/api/v1/match/"
 
 # Flow of orchestrator for streaming:
 # 1. Retrieve match from match service
@@ -27,7 +27,7 @@ match_url = "http://kong:8000/api/v1/match/"
 # class Match(graphene.ObjectType):
 #     id = graphene.String()
 #     name = graphene.String()
-    # Add more fields as needed
+# Add more fields as needed
 
 
 # class Query(graphene.ObjectType):
@@ -50,15 +50,16 @@ match_url = "http://kong:8000/api/v1/match/"
 
 def retrieve_video_url(match_id):
     print("entering retrieve_video_url")
-    url = videoasset_url + "video?id=" + match_id
+    url = VIDEOASS_URL + "video?id=" + match_id
     response = requests.post(url)
     if response.status_code == 200:
         print("response received from video asset ms")
         video_url = response.json()
         return video_url
     else:
+        # TODO: In match microservice, add an endpoint to update the match_id with a default video URL, if it did not previously exist.
         create_video_url = requests.post(
-            match_url + "match-video-url", json={"id": match_id}
+            MATCH_URL + "match-video-url", json={"id": match_id}
         )
         if create_video_url.status_code == 200:
             video_url = create_video_url.json()
@@ -67,33 +68,49 @@ def retrieve_video_url(match_id):
             return "Video URL not found or invalid."
 
 
-@app.route("/<string:id>")
+# @app.route("/<string:id>")
+@app.route("/retrieve/<string:id>")
 def retrieve_match(id):
     # Retrieve match from match service using GraphQL
-    query = '''
-    {
-        match_details(_id: "%s") {
-            _id
-            name
-            description
-            venue
-            home_team
-            away_team
-            home_score
-            away_score
-            date
-            seats
+    query = """
+        query getMatchDetails($id: String) {
+            match_details(_id: $id) {
+                _id
+                name
+                description
+                venue
+                home_team
+                away_team
+                home_score
+                away_score
+                date
+            }
         }
-    }
-    ''' % id
+    """
 
-    url = match_url + ""
-    response = requests.post(url, json={'query': query})
-    if response.status_code == 200:
+    variables = {
+        "id": id,
+    }
+
+    try:
+        response = requests.post(
+            MATCH_URL,
+            json={"query": query, "variables": variables},
+            headers={"Content-Type": "application/json"},
+        )
+
+        if response.status_code == 200:
             match_data = response.json()
-            return match_data['data']['match_details']
-    else:
-            return "Match not found"
+            print("Match data response: ", match_data)
+            return match_data
+            # if match_data["data"]["match_details"]:
+            #     return match_data["data"]["match_details"]
+            # else:
+            #     return "Match not found", 404
+        else:
+            return f"Error retrieving match: {response.status_code}", 500
+    except requests.exceptions.RequestException as e:
+        return f"Error retrieving match: {str(e)}", 500
 
 
 # AMQP consumer
