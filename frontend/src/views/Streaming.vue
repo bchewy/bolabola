1e0b53a491ae752063572bb7ef0e4e3838f63629
MatthiasLJK <jklee.2022@scis.smu.edu.sg>
2024-03-24 16:55:07 +0800

    Update streaming vue
    update streaming vue, removed some unused functions

--- frontend/src/views/Streaming.vue
+++ frontend/src/views/Streaming.vue
@@ -2,7 +2,6 @@
   <div>
     <NavBar />
     <div class="streaming-view">
-      <!-- <p>{{ matchID }}</p> -->
       <div v-if="streamUrl">
         <p> {{ match.name }} </p>
         <VueBasicPlayer :src="streamUrl" :play="playNow"></VueBasicPlayer>
@@ -57,8 +56,6 @@
       };
     },
     mounted() {
-      this.fetchMatches(this.matchID);
-      // this.getMatchStreamingMatchDetails(matchId);
 
       axios
         .get("http://localhost:8000/api/v1/videoasset/video?id=1")
@@ -81,64 +78,13 @@
           .then((response) => {
            console.log(response.data.data.match_details)
            this.match = response.data.data.match_details
-          //   this.matches = response.data.data.matches_overview.map((match) => {
-          //     // Assuming your backend returns an array of match details
-          //     // You might need to adjust based on the actual structure
-          //     console.log(match);
-          //     return {
-          //       id: match._id, // Adjust based on your data structure
-          //       title: `${match.home_team} vs ${match.away_team}`,
-          //       home_team: match.home_team,
-          //       away_team: match.away_team,
-          //       home_score: match.home_score,
-          //       away_score: match.away_score,
-          //       date: new Date(parseInt(match.date)).toLocaleString(), // Convert timestamp to readable date
-          //       description: match.description,
-          //       venue: match.venue,
-          //       seats: match.seats,
-          //     };
-          //   });
           })
           .catch((error) => {
             console.error("Error fetching matches:", error);
           });
     },
     methods: {
-      // fetchMatchDetails(matchId) {
-      //   console.log('Match ID:', matchId);
-      //   axios.get(`http://localhost:8000/api/v1/match/${matchId}`)
-      //     .then(response => {
-      //       const match = response.data;
-      //       this.match = {
-      //         id: match._id,
-      //         title: `${match.home_team} vs ${match.away_team}`,
-      //         home_team: match.home_team,
-      //         away_team: match.away_team,
-      //         date: new Date(parseInt(match.date)).toLocaleString(),
-      //         seats: match.seats,
-      //       };
-      //     })
-      //     .catch(error => {
-      //       console.error('Error fetching match details:', error);
-      //     })
-      //     .finally(() => {
-      //       this.loading = false;
-      //     });
-      // },
-      fetchMatches() {
-      },
-      // getMatchStreamingMatchDetails(matchId) {
-      //   axios
-      //     .get("http://localhost:8000/api/v1/videoasset/video?id=1")
-      //     .then((response) => {
-      //       const streamingDetails = response.data;
-      //       // Process the streaming details as needed
-      //       console.log("Streaming details:", streamingDetails);
-      //     })
-      //     .catch((error) => {
-      //       console.error("Error retrieving streaming details:", error);
-      //     });
-      // },
+
     },
   });
 </script>
