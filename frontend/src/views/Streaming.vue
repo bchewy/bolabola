da41a2018afe2f774b7d574b59a09ee5ae5a1b7d
MatthiasLJK <jklee.2022@scis.smu.edu.sg>
2024-03-30 10:36:43 +0800

    sport ticker

--- frontend/src/views/Streaming.vue
+++ frontend/src/views/Streaming.vue
@@ -3,8 +3,26 @@
     <NavBar />
     <div class="streaming-view">
       <div v-if="streamUrl">
-        <p> {{ match.name }} </p>
-        <VueBasicPlayer :src="streamUrl" :play="playNow" @video-timestamp="handleTimeUpdate" />
+        <h3> {{ match.name }} </h3>
+        <p class><span class="teamName">{{match.home_team }}</span> {{ score[match.home_team] }}:{{ score[match.away_team] }} <span>{{ match.away_team }}</span></p>
+        <!-- <transition-group name="list"> -->
+          <div class="ticker">
+            <p v-for="(highlight,key) in matchInfo " :id="key" >
+              <div :style="{float: match.home_team == highlight.team ? 'left' : 'right', clear: 'both', width:'50%'}">
+                <div class="event">
+                  <p v-if="highlight.team == match.away_team">{{ highlight.timestamp }}'</p>
+                  <div class="eventDetails">
+                    <h6 class="eventHeader">{{ highlight.player}}({{ highlight.team }})</h6>
+                    <p class="eventInfo">{{ highlight.event }}</p>
+                  </div>
+                  <p v-if="highlight.team == match.home_team">{{ highlight.timestamp }}'</p>
+                </div>
+              </div>
+            </p>
+          </div>
+        <div class="videoPlayer">
+          <VueBasicPlayer :match="match" :src="streamUrl" :play="playNow" @video-timestamp="handleTimeUpdate" />
+        </div>
       <p> {{ match.description }}</p>
       </div>
       <div v-else-if="loading">
@@ -55,6 +73,8 @@
         streamUrl: "",
         playNow: true,
         socket: null,
+        matchInfo: [],
+        score:{}
       };
     },
     mounted() {
@@ -63,11 +83,6 @@
         transports: ['websocket'],
         path: '/api/v1/streaming/socket.io',
       });
-
-  
-      // this.socket.on('connect_error', (error) => {
-      //   console.error('Connection error:', error);
-      // });
 
       this.socket.on('disconnect', () => {
         console.log('Disconnected from streaming server');
@@ -78,7 +93,29 @@
       });
 
       this.socket.on('stream', (data) => {
+        this.matchInfo.unshift(data.data);
         console.log('Stream data:', data.data);
+        console.log(data.data.event)
+        if(data.data.event == 'GOAL'){
+          console.log(this.score[data.data.team])
+          this.score[data.data.team] +=1;
+        }
+
+        this.$nextTick(() => {
+      const newEvent = this.$el.querySelector('.event');
+      newEvent.classList.add('animate');
+      const existingEvents = this.$el.querySelectorAll('.event:not(.animate)');
+      existingEvents.forEach((event) => {
+        event.classList.add('slide-down');
+        setTimeout(() => {
+          event.classList.remove('slide-down');
+        }, 1000); // The duration of your animation
+      });
+      setTimeout(() => {
+        newEvent.classList.remove('animate');
+      }, 1000); // The duration of your animation
+    });
+
       });
 
       axios
@@ -102,6 +139,10 @@
           .then((response) => {
             console.log(response.data.data.match_details)
             this.match = response.data.data.match_details;
+            this.score = {
+            [this.match.home_team]:0,
+            [this.match.away_team]:0,
+          }
           })
           .catch((error) => {
             console.error("Error fetching matches:", error);
@@ -119,6 +160,7 @@
 <style scoped>
   .streaming-view {
     text-align: center;
+    align-items: center;
     margin-top: 50px;
   }
 
@@ -129,4 +171,65 @@
   .match-details {
     margin-top: 20px;
   }
+  
+  .videoplayer{
+    align-items: center;
+    width: fit-content;
+    margin: auto;
+  }
+
+  .ticker {
+    margin:auto;
+    /* background-color: grey; */
+    overflow-y: scroll;
+    height: 400px;
+    width:50%;
+  }
+  .eventDetails{
+    width:inherit;
+    margin-left: 3%;
+    margin-right: 3%;
+  }
+  .list-move {
+  transition: transform 1s;
+}
+  .event{
+    margin-top: 2%;
+    margin-bottom: 2%;
+    padding: 5% 5% 0% 5%;
+    border-radius: 15px;
+    background-color: #5356FF;
+    /* width: 50%; */
+    display: flex;
+    justify-content: space-between;
+    transition: transform 1s ease-out;
+  }
+
+.event.animate {
+animation: slideIn 1s ease-out;
+}
+
+.event.slide-down {
+  animation: slideDown 1s ease-out;
+}
+
+@keyframes slideIn {
+  0% {
+    transform: translateY(-100%);
+    opacity: 0;
+  }
+  100% {
+    transform: translateY(0);
+    opacity: 1;
+  }
+}
+
+@keyframes slideDown {
+  0% {
+    transform: translateY(0);
+  }
+  100% {
+    transform: translateY(0%);
+  }
+}
 </style>
